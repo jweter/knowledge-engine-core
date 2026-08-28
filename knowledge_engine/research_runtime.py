20c26c3f348470ce7ac2b2102d6007351bd84801
Jeremy Weter <jeremyweter@gmail.com>
2026-08-28 18:33:30 -0500

    Compose slim extraction and grounding review commands

--- knowledge_engine/research_runtime.py
+++ knowledge_engine/research_runtime.py
@@ -9,12 +9,14 @@ from knowledge_engine.research_command_surface import (
     app,
     research_runtime_capability_payload,
 )
+from knowledge_engine.research_review_surface import register_research_review_commands
 from knowledge_engine.research_secondary_acquisition_surface import (
     register_research_secondary_acquisition_commands,
 )
 
 register_research_oa_acquisition_commands(app)
 register_research_secondary_acquisition_commands(app)
+register_research_review_commands(app)
 
 __all__ = [
     "RESEARCH_RUNTIME_CONTRACT_VERSION",
