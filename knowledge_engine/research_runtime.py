b6294083337815030fdccb425dbc96c241d038bc
Jeremy Weter <jeremyweter@gmail.com>
2026-08-28 18:30:11 -0500

    Compose slim research runtime command groups

---
+++ knowledge_engine/research_runtime.py
@@ -0,0 +1,20 @@
+"""Composed entry point for the staged hosted Research Copilot runtime."""
+
+from knowledge_engine.research_acquisition_surface import (
+    register_research_oa_acquisition_commands,
+)
+from knowledge_engine.research_command_surface import (
+    RESEARCH_RUNTIME_CONTRACT_VERSION,
+    RESEARCH_RUNTIME_REQUIRED_COMMANDS,
+    app,
+    research_runtime_capability_payload,
+)
+
+register_research_oa_acquisition_commands(app)
+
+__all__ = [
+    "RESEARCH_RUNTIME_CONTRACT_VERSION",
+    "RESEARCH_RUNTIME_REQUIRED_COMMANDS",
+    "app",
+    "research_runtime_capability_payload",
+]
