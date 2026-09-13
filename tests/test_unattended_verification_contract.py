f864a129a21e6bc1b4e9a9de5612bc50494b1f53
Jeremy Weter <jeremyweter@gmail.com>
2026-09-13 08:32:23 -0500

    style: fix worker contract import ordering

--- tests/test_unattended_verification_contract.py
+++ tests/test_unattended_verification_contract.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
-import pytest
 from pydantic import ValidationError
+import pytest
 
 from knowledge_engine.unattended_verification_contract import WorkerRequest, WorkerResult
 
